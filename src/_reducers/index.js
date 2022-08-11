fb5c2a9026bfbf7a0ec112db499e835caa0f6bf8
Jieun Oh <65931227+0909oje@users.noreply.github.com>
2022-08-11 15:02:28 +0900

    edit) user_reducer function

--- src/_reducers/index.js
+++ src/_reducers/index.js
@@ -1,3 +1,9 @@
+// 리듀서가 두개 이상이면 합쳐야함.
+// 여기서는 리듀서가 COVID_MAIN에 대한 것 하나밖에 없으므로
+// 사실상 combine을 하지 않아도 되지만
+// 통념상 여러 개의 리듀서를 합치는 작업을 하므로
+// 하나의 리듀서를 root 리듀서로 씀을 적어둠.
+
 import {combineReducers} from 'redux';
 import user from './user_reducer';
 
--- src/_reducers/user_reducer.js
+++ src/_reducers/user_reducer.js
@@ -1,14 +1,35 @@
 import { COVID_MAIN } from "../_actions/type";
 
-export default function (state={},action){
+export default function user(state={},action){
     switch (action.type){
         case COVID_MAIN:
-                return {...state,loginSuccess:action.payload};
+            return {
+                ...state,
+                loginSuccess:action.payload
+            };
             break;
 
         default:
             return state;
-
-        
     }
 }
+
+// const initialState=[
+
+// ];
+
+// export default function todos(state = initialState, action){
+//     switch(action.type){
+//         case ADD_TODO:
+//             return state.concat(action.todo);
+//         case TOGGLE_TODO:
+//             return state.map(
+//                 todo =>
+//                     todo.id === action.id
+//                     ? {...todo, done: !todo.done}
+//                     : todo
+//             );
+//         default:
+//             return state;
+//     }
+// }
